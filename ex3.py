46e1fff0eb6f3aa34a025db7cd6ea83c3588ad05
keguoh <timmy0703@gmail.com>
2016-05-24 00:11:30 -0500

    ex

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# We will count the chickens
+print "I will now count my chickens:"
+
+# The following are hens and roosters
+print "Hens", 25 + 30 / 6
+print "Rooster", 100 - 25 * 3 % 4
+
+# counting eggs 
+print "Now I will count the eggs:"
+
+# # of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# which one is greater?
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Which is bigger?
+print 3 + 2 < 5 - 7
+
+# What are they?
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# Why is false?
+print "Oh, that's why it's False."
+
+# Wanna some more
+print "How about some more."
+
+# relations.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
