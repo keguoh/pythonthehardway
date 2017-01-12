19f7ed66675c96889dd1dba46448a1b40f093709
Keguo Huang <timmy0703@gmail.com>
2017-01-11 23:02:50 -0600

    class - instance

--- ex40.py
+++ ex40.py
@@ -26,3 +26,24 @@ bulls_on_parade = Song(["They rally around the family", "With pockets full of sh
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+
+
+
+
+# https://www.youtube.com/watch?v=trOZBgZ8F_c
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+    def eat(self, food):
+        if food == 'apple':
+            self.health -= 100
+        elif food == 'ham':
+            self.health += 20
+Bob = Hero('Bob')
+print Bob.name
+print Bob.health
+Bob.eat('apple')
+print Bob.health
+Bob.eat('ham')
+print Bob.health
